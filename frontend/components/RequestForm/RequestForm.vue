6f787a4abe1389430b1c9974113e6d3035a88b27
ozi <zaytsevoi.90@yandex.ru>
2024-06-13 13:26:42 +1200

    feat: добавляет выбор даты и маршрута

--- frontend/components/RequestForm/RequestForm.vue
+++ frontend/components/RequestForm/RequestForm.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts" generic="TData">
 import { toTypedSchema } from '@vee-validate/zod';
-import { string, object } from 'zod';
+import { string, object, number } from 'zod';
 import { useToast } from '../ui/toast/use-toast';
 import type { ModalProps, Passenger } from '~/types';
 import type { Table } from '@tanstack/vue-table';
 import { genders, categories } from '~/data';
 
-import { DateFormatter, getLocalTimeZone, parseDate, today } from '@internationalized/date'
-import { toDate } from 'radix-vue/date'
-import { cn } from '@/lib/utils'
+import { DateFormatter, getLocalTimeZone, parseDate, today } from '@internationalized/date';
+import { toDate } from 'radix-vue/date';
+import { cn } from '@/lib/utils';
 
 const { toast } = useToast();
 
@@ -16,20 +16,24 @@ const selected = useSelectedRow();
 const table = inject<Table<TData>>('table');
 const isDisabled = ref<boolean>(false);
 const isLoading = ref<boolean>(false);
+const isLoadingPath = ref<boolean>(false);
+const isDisableTimeEnd = ref<boolean>(true);
 const open = ref(false);
-const placeholder = ref()
+const placeholder = ref();
+const path = ref('');
 
 const df = new DateFormatter('ru-Ru', {
   dateStyle: 'long',
-})
+});
 
 const value = computed({
-  get: () => values.date ? parseDate(values.date) : undefined,
-  set: val => val,
-})
+  get: () => (values.date ? parseDate(values.date) : undefined),
+  set: (val) => val,
+});
 
 const formSchema = toTypedSchema(
   object({
+    passenger: number().optional(),
     first_name: string({ required_error: 'Поле не должно быть пустым' }).trim(),
     second_name: string({ required_error: 'Поле не должно быть пустым' }).trim(),
     patronymic: string({ required_error: 'Поле не должно быть пустым' }).trim(),
@@ -64,7 +68,13 @@ const formSchema = toTypedSchema(
           return null;
         }
       }),
-    date: string().refine(v => v, { message: 'A date of birth is required.' }),
+    date: string({ required_error: 'Выберите дату поездки' }).refine((v) => v, { message: 'Выберите дату поездки' }),
+    time_start: string({ required_error: 'Заполните время начала поездки' })
+      .min(5, 'Заполните время начала поездки')
+      .trim(),
+    time_end: string().trim().optional(),
+    from_station: string({ required_error: 'Выберите станцию начала поездки' }).trim(),
+    to_station: string({ required_error: 'Выберите конца поездки' }).trim(),
   }),
 );
 
@@ -112,7 +122,33 @@ const modal: ModalProps = {
   },
 };
 
+const getPath = async () => {
+  if (
+    (values.from_station !== undefined && values.to_station === undefined) ||
+    (values.to_station !== undefined && values.from_station === undefined)
+  ) {
+    return;
+  }
+
+  isLoadingPath.value = !isLoadingPath.value;
+  isDisableTimeEnd.value = true
+  
+  const isPath: string = await new Promise((res, rej) => {
+    setTimeout(() => {
+      res('Капуууучииноо!');
+      isLoadingPath.value = !isLoadingPath.value;
+      isDisableTimeEnd.value = false
+    }, 1000);
+  });
+
+  path.value = isPath;
+  
+};
+
 const openModal = () => {
+  path.value = '';
+  isDisableTimeEnd.value = true
+
   if (!table) {
     return;
   }
@@ -120,10 +156,11 @@ const openModal = () => {
   const { rows } = table.getFilteredSelectedRowModel();
 
   if (rows.length) {
-    const { first_name, second_name, patronymic, category, phone, gender, is_pacemaker }: Passenger = rows[0]
+    const { first_name, second_name, patronymic, category, phone, gender, is_pacemaker, id }: Passenger = rows[0]
       .original as Passenger;
 
     setValues({
+      passenger: +id,
       first_name,
       second_name,
       patronymic,
@@ -194,8 +231,8 @@ onBeforeRouteLeave(() => {
               </UiFormControl>
               <UiSelectContent>
                 <UiSelectGroup>
-                  <UiSelectItem v-for="category in categories" :key="category.id" :value="`${category.id}`">{{
-                    category.code }}
+                  <UiSelectItem v-for="category in categories" :key="category.id" :value="`${category.id}`"
+                    >{{ category.code }}
                   </UiSelectItem>
                 </UiSelectGroup>
               </UiSelectContent>
@@ -246,7 +283,8 @@ onBeforeRouteLeave(() => {
               </UiFormControl>
               <UiSelectContent>
                 <UiSelectGroup>
-                  <UiSelectItem v-for="gender in genders" :key="gender.id" :value="`${gender.value}`">{{ gender.name }}
+                  <UiSelectItem v-for="gender in genders" :key="gender.id" :value="`${gender.value}`"
+                    >{{ gender.name }}
                   </UiSelectItem>
                 </UiSelectGroup>
               </UiSelectContent>
@@ -256,72 +294,214 @@ onBeforeRouteLeave(() => {
         </UiFormField>
       </div>
 
-      <UiFormField v-slot="{ componentField }" name="description">
-        <UiFormItem>
-          <UiFormLabel>Комментарий</UiFormLabel>
-          <UiFormControl>
-            <UiTextarea placeholder="Введите комментарий" class="resize-none mb-7" v-bind="componentField" />
-          </UiFormControl>
-          <UiFormMessage />
-        </UiFormItem>
-      </UiFormField>
-
-      <hr />
+      <UiSeparator />
 
       <div class="request-form__trip pt-6">
         <div class="request-form__title">
           <h2>Поездка</h2>
         </div>
 
-        <div class="request-form__trip-fields">
+        <div class="request-form__trip-fields grid gap-y-5">
           <UiFormField name="date">
-            <UiFormItem class="flex flex-col">
+            <UiFormItem class="flex flex-col w-[360px]">
               <UiFormLabel>Дата</UiFormLabel>
               <UiPopover>
                 <PopoverTrigger as-child>
                   <UiFormControl>
-                    <UiButton variant="outline" :class="cn(
-                      'ps-3 text-start font-normal justify-normal',
-                      !value && 'text-muted-foreground',
-                    )">
-                      <span>{{ value ? df.format(toDate(value)) : "Выберите дату" }}</span>
+                    <UiButton
+                      variant="outline"
+                      :class="cn('ps-3 text-start font-normal justify-normal', !value && 'text-muted-foreground')"
+                    >
+                      <span>{{ value ? df.format(toDate(value)) : 'Выберите дату' }}</span>
                       <Icon class="ml-auto" name="ion:calendar-outline" />
-
                     </UiButton>
-                    <input hidden>
+                    <input hidden />
                   </UiFormControl>
                 </PopoverTrigger>
-                <UiPopoverContent class="w-auto p-0">
-                  <UiCalendar v-model:placeholder="placeholder" v-model="value" calendar-label="Дата" locale="ru-RU"
-                    initial-focus :min-value="today(getLocalTimeZone())" @update:model-value="(v) => {
-                      if (v) {
-                        setFieldValue('date', v.toString())
+                <UiPopoverContent class="p-0">
+                  <UiCalendar
+                    v-model:placeholder="placeholder"
+                    v-model="value"
+                    calendar-label="Дата"
+                    locale="ru-RU"
+                    initial-focus
+                    :min-value="today(getLocalTimeZone())"
+                    @update:model-value="
+                      (v) => {
+                        if (v) {
+                          setFieldValue('date', v.toString());
+                        } else {
+                          setFieldValue('date', undefined);
+                        }
                       }
-                      else {
-                        setFieldValue('date', undefined)
-                      }
-
-                    }" />
+                    "
+                  />
                 </UiPopoverContent>
               </UiPopover>
               <UiFormMessage />
             </UiFormItem>
           </UiFormField>
 
+          <div class="flex w-full gap-x-5">
+            <UiFormField v-slot="{ componentField }" name="time_start">
+              <UiFormItem class="flex flex-col w-auto">
+                <UiFormLabel>Время начала</UiFormLabel>
+                <UiFormControl>
+                  <UiInput class="w-max" type="time" v-bind="componentField" />
+                </UiFormControl>
+                <UiFormMessage />
+              </UiFormItem>
+            </UiFormField>
+
+            <UiFormField v-slot="{ componentField }" name="time_end">
+              <UiFormItem class="flex flex-col w-auto">
+                <UiFormLabel>Время окончания (заполнится автоматически)</UiFormLabel>
+                <UiFormControl>
+                  <UiInput class="w-max" type="time" v-bind="componentField" :disabled="isDisableTimeEnd" />
+                </UiFormControl>
+                <UiFormMessage />
+              </UiFormItem>
+            </UiFormField>
+          </div>
+
+          <div class="grid grid-cols-2 gap-x-5">
+            <UiFormField name="from_station">
+              <UiFormItem class="flex flex-col">
+                <UiFormLabel>Откуда</UiFormLabel>
+                <UiPopover>
+                  <UiPopoverTrigger as-child>
+                    <UiFormControl>
+                      <UiButton
+                        variant="outline"
+                        role="combobox"
+                        :class="cn('justify-between', !values.from_station && 'text-muted-foreground')"
+                      >
+                        {{
+                          values.from_station
+                            ? categories.find((category) => category.code === values.from_station)?.code
+                            : 'Выберите странцию'
+                        }}
+                        <LucideChevronsUpDown class="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                      </UiButton>
+                    </UiFormControl>
+                  </UiPopoverTrigger>
+                  <UiPopoverContent class="p-0">
+                    <UiCommand>
+                      <UiCommandInput placeholder="Выберите странцию" />
+                      <UiCommandEmpty>Станции не найдено</UiCommandEmpty>
+                      <UiCommandList>
+                        <UiCommandGroup>
+                          <UiCommandItem
+                            v-for="category in categories"
+                            :key="category.id"
+                            :value="category.code"
+                            @select="
+                              () => {
+                                setFieldValue('from_station', category.code);
+                                getPath();
+                              }
+                            "
+                          >
+                            <LucideCheck
+                              :class="
+                                cn('mr-2 h-4 w-4', category.code === values.from_station ? 'opacity-100' : 'opacity-0')
+                              "
+                            />
+                            {{ category.code }}
+                          </UiCommandItem>
+                        </UiCommandGroup>
+                      </UiCommandList>
+                    </UiCommand>
+                  </UiPopoverContent>
+                </UiPopover>
+                <UiFormMessage />
+              </UiFormItem>
+            </UiFormField>
+
+            <UiFormField name="to_station">
+              <UiFormItem class="flex flex-col">
+                <UiFormLabel>Куда</UiFormLabel>
+                <UiPopover>
+                  <UiPopoverTrigger as-child>
+                    <UiFormControl>
+                      <UiButton
+                        variant="outline"
+                        role="combobox"
+                        :class="cn('justify-between', !values.to_station && 'text-muted-foreground')"
+                      >
+                        {{
+                          values.to_station
+                            ? categories.find((category) => category.code === values.to_station)?.code
+                            : 'Выберите странцию'
+                        }}
+                        <LucideChevronsUpDown class="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                      </UiButton>
+                    </UiFormControl>
+                  </UiPopoverTrigger>
+                  <UiPopoverContent class="p-0">
+                    <UiCommand>
+                      <UiCommandInput placeholder="Выберите странцию" />
+                      <UiCommandEmpty>Станции не найдено</UiCommandEmpty>
+                      <UiCommandList>
+                        <UiCommandGroup>
+                          <UiCommandItem
+                            v-for="category in categories"
+                            :key="category.id"
+                            :value="category.code"
+                            @select="
+                              () => {
+                                setFieldValue('to_station', category.code);
+                                getPath();
+                              }
+                            "
+                          >
+                            <LucideCheck
+                              :class="
+                                cn('mr-2 h-4 w-4', category.code === values.to_station ? 'opacity-100' : 'opacity-0')
+                              "
+                            />
+                            {{ category.code }}
+                          </UiCommandItem>
+                        </UiCommandGroup>
+                      </UiCommandList>
+                    </UiCommand>
+                  </UiPopoverContent>
+                </UiPopover>
+                <UiFormMessage />
+              </UiFormItem>
+            </UiFormField>
+          </div>
+
+          <p class="p-3 bg-[#F7F9FA] rounded-md flex text-muted-foreground">
+            <span v-if="!isLoadingPath && path">{{ path }}</span>
+            <span v-else-if="!isLoadingPath && !path">Маршрут пока не выбран</span>
+            <span v-else class="flex items-center">
+              <LucideLoader2 class="w-4 h-4 mr-2 animate-spin" /> Пожалуйста подождите
+            </span>
+          </p>
+
+          <UiFormField v-slot="{ componentField }" name="description">
+            <UiFormItem>
+              <UiFormLabel>Комментарий</UiFormLabel>
+              <UiFormControl>
+                <UiTextarea placeholder="Введите комментарий" class="resize-none mb-7" v-bind="componentField" />
+              </UiFormControl>
+              <UiFormMessage />
+            </UiFormItem>
+          </UiFormField>
         </div>
       </div>
 
       <UiDialogFooter class="mb-5">
         <UiDialogClose
           class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border bg-background hover:bg-accent h-10 px-4 py-2 text-[#3A76EE] hover:text-[#3A76EE] border-[#3976EE]"
-          type="button">
+          type="button"
+        >
           Закрыть без сохранения
         </UiDialogClose>
         <UiButton v-auto-animate="{ duration: 150 }" class="passanger-form__submit" type="submit" :disabled="isLoading">
-          <span v-if="!isLoading"> Сохранить </span>
-          <span v-else>
-            <LucideLoader2 class="w-4 h-4 mr-2 animate-spin" /> Пожалуйста подождите
-          </span>
+          <span v-if="!isLoading">Сохранить</span>
+          <span v-else> <LucideLoader2 class="w-4 h-4 mr-2 animate-spin" /> Пожалуйста подождите </span>
         </UiButton>
       </UiDialogFooter>
     </form>
@@ -343,7 +523,7 @@ onBeforeRouteLeave(() => {
   & label {
     @apply text-[#8E8E8E] mb-1 block;
 
-    &+button {
+    & + button {
       @apply bg-[#F7F9FA] font-normal text-base;
     }
   }
